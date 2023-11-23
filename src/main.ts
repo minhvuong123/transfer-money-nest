5f8eb48f1a7f6ce9e1c9d64998a116c8f64cbc2c
minhvuong123 <minhvuong12222@gmail.com>
2023-11-24 02:04:25 +0700

    implement session store with connect-typeorm

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
 import * as session from 'express-session';
+import { DataSource } from 'typeorm';
+import { Session } from './utils/typeorm';
+import { TypeormStore } from 'connect-typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +13,9 @@ async function bootstrap() {
   app.enableCors({ origin: ['https://localhost:3000'], credentials: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+
+  const dataSource = app.get(DataSource);
+  const sessionReposutory = dataSource.getRepository(Session);
 
   app.use(
     session({
@@ -20,6 +26,7 @@ async function bootstrap() {
       cookie: {
         maxAge: 3600000 * 24,
       },
+      store: new TypeormStore().connect(sessionReposutory),
     }),
   );
   app.use(passport.initialize());
